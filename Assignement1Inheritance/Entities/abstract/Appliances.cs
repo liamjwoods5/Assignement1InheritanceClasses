dcde7dc64c1fd96af2b9eb630914e0e5b709353c
liamjwoods5 <144362734+liamjwoods5@users.noreply.github.com>
2024-02-11 13:15:48 -0700

    modernappliances

--- Assignement1Inheritance/Entities/abstract/Appliances.cs
+++ Assignement1Inheritance/Entities/abstract/Appliances.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignement1Inheritance.Entities.@abstract
+﻿namespace Assignement1Inheritance.Entities.@abstract
 {
     public abstract class Appliance
     {
