e716b77b87e10d7abd1c7d5eb6aa65060b52c519
liamjwoods5 <144362734+liamjwoods5@users.noreply.github.com>
2024-02-11 13:17:11 -0700

    Revert "modernappliances"
    This reverts commit dcde7dc64c1fd96af2b9eb630914e0e5b709353c.

--- Assignement1Inheritance/Entities/abstract/Appliances.cs
+++ Assignement1Inheritance/Entities/abstract/Appliances.cs
@@ -1,4 +1,11 @@
-﻿namespace Assignement1Inheritance.Entities.@abstract
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement1Inheritance.Entities.@abstract
 {
     public abstract class Appliance
     {
--- Assignement1Inheritance/Entities/abstract/Dishwasher.cs
+++ Assignement1Inheritance/Entities/abstract/Dishwasher.cs
@@ -1,4 +1,10 @@
-﻿namespace Assignement1Inheritance.Entities.@abstract
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement1Inheritance.Entities.@abstract
 {
     internal class Dishwasher : Appliance
     {
